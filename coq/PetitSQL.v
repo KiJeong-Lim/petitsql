4dbea0ae06cc8e7f8b27c30190e225227b91c07d
KiJeong-Lim <gijungdduk@naver.com>
2022-10-31 20:19:01 +0900

    .

--- coq/PetitSQL.v
+++ coq/PetitSQL.v
@@ -6,8 +6,62 @@ Require Import Coq.Strings.String.
 Require Import Coq.Program.Program.
 Require Import Coq.micromega.Lia.
 Require Import Coq.Arith.PeanoNat.
+Require Import Coq.Lists.List.
 
-Module Hs.
+Module T. (* For Haskell types *)
+
+  Definition Char : Set := ascii.
+
+  Definition String : Set := string.
+
+  Definition Parser (a : Type) : Type := T.String -> list (a * T.String).
+
+End T.
+
+Module P. (* For petitsql/app/ParserLib.hs *)
+
+  Import ListNotations IfNotations.
+
+  Definition pure {A : Type} (v : A) : T.Parser A :=
+    fun inp => [(v,inp)].
+
+  Definition failure {A : Type} : T.Parser A :=
+    fun inp => [].
+
+  Definition item : T.Parser T.Char :=
+    fun inp =>
+    match inp with
+    | EmptyString => []
+    | String x xs => [(x,xs)]
+    end.
+
+  Definition parse {A : Type} (p : T.Parser A) (inp : T.String) : list (A * T.String) := p inp.
+
+  Definition bind {A : Type} {B : Type} (m : T.Parser A) (k : A -> T.Parser B) : T.Parser B :=
+    fun inp => concat (map (fun '(v,out) => k v out) (m inp)).
+
+  Global Infix " >>= " := bind (left associativity, at level 90).
+
+  Definition alt {A : Type} (p : T.Parser A) (q : T.Parser A) : T.Parser A :=
+    fun inp => parse p inp ++ parse q inp.
+
+  Global Infix " +++ " := alt (left associativity, at level 50).
+
+  Definition sat (p : T.Char -> bool) : T.Parser T.Char :=
+    item >>= fun x => if p x then pure x else failure.
+
+(** Fail to define: -- Cannot guess decreasing argument of fix.
+
+  Fixpoint many {A : Type} (p : T.Parser A) : T.Parser (list A) :=
+    many1 p +++ pure []
+  with many1 {A : Type} (p : T.Parser A) : T.Parser (list A) :=
+    p >>= fun v => many p >>= fun vs => pure (v :: vs).
+
+*)
+
+End P.
+
+Module Hs. (* For petitsql/app/PetitSQL.hs *)
 
   Import IfNotations.
 
